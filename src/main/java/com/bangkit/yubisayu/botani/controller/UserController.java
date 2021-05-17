cf3790d02182d3fc285346c82c60e8d0585a47ba
anugrah prasetia <anugrahp99@student.ub.ac.id>
2021-05-17 23:29:01 +0700

    create controllers

---
+++ src/main/java/com/bangkit/yubisayu/botani/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.bangkit.yubisayu.botani.controller;
+
+import com.bangkit.yubisayu.botani.constants.ApiPath;
+import com.bangkit.yubisayu.botani.model.entities.user.User;
+import com.bangkit.yubisayu.botani.service.UserService;
+import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@Api
+@RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping(ApiPath.USERS_ID)
+    public Mono<User> getCustomerById(@PathVariable String id) throws Exception {
+        return userService.getUserById(id);
+    }
+}
